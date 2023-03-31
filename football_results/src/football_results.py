cbd4958d1ffbc40fa46ee423fb07f49044340543
Ewan G <ewang@ITs-MacBook-Pro.local>
2023-03-31 11:11:03 +0100

    football result solution

--- football_results/src/football_results.py
+++ football_results/src/football_results.py
@@ -1,6 +1,17 @@
+# {
+#                 "Home_score": 3,
+#                 "Away_score": 1
+#             # },
+
 def get_result(final_score):
-    pass
+    if final_score["Home_score"] > final_score["Away_score"]:
+        return "Home win"
+    elif final_score["Home_score"] < final_score["Away_score"]:
+        return "Away win"
+    elif final_score["Home_score"] == final_score["Away_score"]:
+        return "Draw"
 
 def get_results(final_scores):
-    pass
+    results = [get_result(score) for score in final_scores]
+    return results
     # (You could try and use a list comprehension for this)
